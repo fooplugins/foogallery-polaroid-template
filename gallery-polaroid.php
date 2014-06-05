0109096494e0a9eb78eafb470f381cf50d79ec5b
Brad Vincent <bradvin@gmail.com>
2014-06-05 16:35:54 +0200

    fixes for template

--- gallery-polaroid.php
+++ gallery-polaroid.php
@@ -8,8 +8,8 @@ $size = foogallery_gallery_template_setting( 'thumbnail_size', 'thumbnail' );
 $link = foogallery_gallery_template_setting( 'thumbnail_link', 'image' );
 $lightbox = foogallery_gallery_template_setting( 'lightbox', 'unknown' );
 ?>
-<ul class="foogallery-container foogallery-polaroid foogallery-lightbox-<?php echo $lightbox; ?>">
+<ul class="foogallery-container foogallery-polaroid foogallery-lightbox-<?php echo esc_attr($lightbox); ?>">
 	<?php foreach ( $current_foogallery->attachments() as $attachment_id ) {
 		echo '<li>' . foogallery_get_attachment_html( $attachment_id, $size, $link ) . '</li>';
 	} ?>
-</ul
+</ul>
